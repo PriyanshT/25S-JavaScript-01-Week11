4d0d882ef5c75e3a6abe51eada7113a71599b205
PriyanshT <priyanshthakar@gmail.com>
2025-07-23 18:46:56 -0400

    poetry complete

--- js/poetry.js
+++ js/poetry.js
@@ -13,16 +13,33 @@ verseChoose.addEventListener("change", function () {
 function updateDisplay(verse) {
     console.log("Verse from inside the function: " + verse);
     // STEP 4: Declare and initialize URL to point to text file(s)
-    let url = "";
+    let url = `https://priyansht.github.io/25S-JavaScript-01-Week11/${verse}.txt`;
+    console.log(url);
     // STEP 5: Build fetch() with promises
     // STEP 5a: Use fetch and pass in the URL
-    // STEP 5b: The fetch() will return a promise - which when received from the server, the promise's then() event handler is called using the response
-    // STEP 5c: If the response is not okay, throw an error containing the HTTP status
-    // STEP 5d: If the response is okay, the handler fetches the response and returns it as text with response.text()
-    // STEP 5e: Once response.text() has returned a value, the then() handler can pass in the text string to the textContent property of the poemDisplay element
-    // STEP 5f: Finish the chain with a catch() to grab any errors that may have been thrown by the promise, and display them on the page
+    fetch(url)
+        // STEP 5b: The fetch() will return a promise - which when received from the server,
+        // the promise's then() event handler is called using the response
+        .then((response) => {
+            console.log(response);
+            // STEP 5c: If the response is not okay, throw an error containing the HTTP status
+            if (!response.ok) {
+                throw new Error("Error Occurred!");
+            }
+            // STEP 5d: If the response is okay, the handler fetches the response and returns it as text with response.text()
+            return response.text();
+        })
+        // STEP 5e: Once response.text() has returned a value, the then() handler can pass in the text string to the textContent property of the poemDisplay element
+        .then((text) => {
+            pre.textContent = text;
+        })
+        // STEP 5f: Finish the chain with a catch() to grab any errors that may have been thrown by the promise, and display them on the page
+        .catch((error) => {
+            pre.textContent = error;
+        });
 }
 
 // STEP 6: Initialize the app with Verse 1
+updateDisplay("verse1");
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
